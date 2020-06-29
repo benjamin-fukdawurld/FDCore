102a3b966100a2e08d40397c92873d01f9afdab1
benjamin <benjamin.zamour@gmail.com>
2020-06-29 22:11:21 +0200

    Turned export macro to cross platform version

--- include/FDCore/Common/Macros.h
+++ include/FDCore/Common/Macros.h
@@ -1,7 +1,14 @@
 #ifndef FDCORE_MACROS_H
 #define FDCORE_MACROS_H
 
-#define FD_EXPORT __attribute__((visibility("default")))
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(__CYGWIN__)
+    #define FD_HAS_DECLSPEC
+    #define FD_EXPORT __attribute__((__dllexport__))
+    #define FD_IMPORT __attribute__((__dllimport__))
+#else
+    #define FD_EXPORT __attribute__((__visibility__("default")))
+    #define FD_IMPORT
+#endif
 
 #ifndef FD_NOINLINE
     #define FD_INLINE inline
