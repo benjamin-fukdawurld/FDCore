670c1d92180ac6e2056e273e2653375b5e3ce0e4
benjamin <benjamin.zamour@gmail.com>
2020-09-21 23:34:55 +0200

    Made AbstractApplication::addPlugin a non abstract method

--- include/FDCore/ApplicationManagement/AbstractApplication.h
+++ include/FDCore/ApplicationManagement/AbstractApplication.h
@@ -2,30 +2,29 @@
 #define FDCORE_ABSTRACTAPPLICATION_H
 
 #include <FDCore/Common/Object.h>
-
 #include <FDCore/PluginManagement/AbstractPlugin.h>
 
 namespace FDCore
 {
     class AbstractApplication : Object
     {
-        public:
-            enum ExitStatus
-            {
-                ExitSuccess = 0,
-                ExitFailure = -1
-            };
+      public:
+        enum ExitStatus
+        {
+            ExitSuccess = 0,
+            ExitFailure = -1
+        };
 
-        public:
-            AbstractApplication(){}
-            virtual ~AbstractApplication() = default;
+      public:
+        AbstractApplication() {}
+        virtual ~AbstractApplication() = default;
 
-            virtual bool init() = 0;
-            virtual void quit() = 0;
-            virtual int run(int argc, char **argv) = 0;
+        virtual bool init() = 0;
+        virtual void quit() = 0;
+        virtual int run(int argc, char **argv) = 0;
 
-            virtual void addPlugin(AbstractPlugin *plugin) = 0;
+        virtual void addPlugin(AbstractPlugin *) {};
     };
-}
+} // namespace FDCore
 
 #endif // FDCORE_ABSTRACTAPPLICATION_H
